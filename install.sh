84ef7dc7ef28ac2651ac10d7c03d65baa09a8114
Eduardo Malherbi Martins <eduardo@malherbi.com>
2016-06-28 02:16:48 -0300

    1.0.0

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
 
-# [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 
-echo "OK"
+echo "$DOTFILES_DIR"
 
 #ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 #ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~

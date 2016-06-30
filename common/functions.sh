5dd8a578a54b7d5287036ce30ad8278a54d02259
emalherbi <emalherbi@gmail.com>
2016-06-30 10:49:12 -0300

    update functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -11,18 +11,18 @@
 #  36  # Cyan    #
 #  37  # White   #
 ##################
-function print {
+function print() {
 	echo  "\033[0;32m $1 \033[0m";
 }
-function print_ln {
+function print_ln() {
 	echo  "\033[1;37m \033[0m";
 	echo  "\033[1;37m=> $1\033[0m";
 	echo  "\033[1;37m \033[0m";
 }
-function print_alert {
+function print_alert() {
 	echo "\033[1;31m✖ $1 ✖\033[0m";
 }
-function print_checking {
+function print_checking() {
 	echo  "\033[1;32m=> $1 ✔\033[0m";
 }
 

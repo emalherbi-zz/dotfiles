631e0831879df92a0b655b48df320d7a3d4c62c7
emalherbi <emalherbi@gmail.com>
2016-06-30 10:54:48 -0300

    add -e on functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -12,18 +12,18 @@
 #  37  # White   #
 ##################
 function print() {
-	echo  "\033[0;32m $1 \033[0m";
+	echo -e "\033[0;32m $1 \033[0m";
 }
 function print_ln() {
-	echo  "\033[1;37m \033[0m";
-	echo  "\033[1;37m=> $1\033[0m";
-	echo  "\033[1;37m \033[0m";
+	echo -e "\033[1;37m \033[0m";
+	echo -e "\033[1;37m=> $1\033[0m";
+	echo -e "\033[1;37m \033[0m";
 }
 function print_alert() {
-	echo "\033[1;31m✖ $1 ✖\033[0m";
+	echo -e "\033[1;31m✖ $1 ✖\033[0m";
 }
 function print_checking() {
-	echo  "\033[1;32m=> $1 ✔\033[0m";
+	echo -e "\033[1;32m=> $1 ✔\033[0m";
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then

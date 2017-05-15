0a3d0dc806b2e7f98510a2bca97eba612962ec5e
Eduardo Malherbi <emalherbi@gmail.com>
2017-05-15 20:10:20 -0300

    close #10

---
+++ atom/global.sh
@@ -0,0 +1,29 @@
+source common/functions.sh
+
+print_install  "atom-beautify" "sudo apm install atom-beautify"
+
+print_install  "autocomplete-plus" "sudo apm install autocomplete-plus"
+
+print_install  "docblockr" "sudo apm install docblockr"
+
+print_install  "editorconfig" "sudo apm install editorconfig"
+
+print_install  "emmet" "sudo apm install emmet"
+
+print_install  "highlight-selected" "sudo apm install highlight-selected"
+
+print_install  "linter-csslint" "sudo apm install linter-csslint"
+
+print_install  "linter-htmlhint" "sudo apm install linter-htmlhint"
+
+print_install  "linter-jshint" "sudo apm install linter-jshint"
+
+print_install  "minimap" "sudo apm install minimap"
+
+print_install  "minimap-highlight-selected" "sudo apm install minimap-highlight-selected"
+
+print_install  "open-recent" "sudo apm install open-recent"
+
+print_install  "todo-show" "sudo apm install todo-show"
+
+print_install  "tree-ignore" "sudo apm install tree-ignore"

a3730713076134998eef17cda46569e7ab5a17ff
Eduardo Malherbi <emalherbi@gmail.com>
2017-06-07 09:28:08 -0300

    close #39

--- common/functions.sh
+++ common/functions.sh
@@ -1,32 +1,36 @@
+#!/bin/bash
+
 function print() {
-	echo -e "\033[0;32m$1\033[0m";
+	echo -e "\033[0;36m $1 \033[0m";
 }
-function print_ln() {
-	echo -e "\033[1;37m \033[0m";
-	echo -e "\033[1;37m=> $1\033[0m";
-	echo -e "\033[1;37m \033[0m";
+
+function print_start() {
+	echo -e "\033[1;36m=> $1\033[0m";
 }
+
 function print_alert() {
 	echo -e "\033[1;31m=> $1 ✖\033[0m";
 }
+
 function print_checking() {
 	echo -e "\033[1;32m=> $1 ✔\033[0m";
 }
+
 function print_install() {
-  while true; do
-      read -p "Do you wish to install '$1'? [Y/n] " yn
-      case $yn in
-          [Yy]* )
-            print "Successfully installed: $1"
-            $2
-			print_checking "$1"
-            break;;
-          [Nn]* )
-            print_alert "Canceled: $1"
-            break;;
-          * ) echo "Please answer Y [Yes] or N [No].";;
-      esac
-  done
+    while true; do
+        read -p "Do you wish to install $1? [Y/n] " yn;
+        case $yn in
+            [Yy]* )
+                print_start "Starting installation: $1";
+                $2;
+                print_checking "Successfully installed: $1";
+                break;;
+            [Nn]* )
+                print_alert "Canceled: $1";
+                break;;
+            * ) echo "Please answer Y [Yes] or N [No].";;
+        esac
+    done
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then
@@ -36,3 +40,5 @@ else
 fi
 
 pwd=$(pwd)
+
+# END

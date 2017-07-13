18fa056fd276383dc18a3aa43a2b8dee7aa98a92
Eduardo Malherbi <emalherbi@gmail.com>
2017-07-12 22:39:44 -0300

    fix #48

--- linux/global.sh
+++ linux/global.sh
@@ -8,8 +8,14 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 
+# aptitude
 print_checking_install  "aptitude" "which aptitude" "sudo apt-get install -y aptitude"
 
 print_checking_install  "xclip" "which xclip" "sudo aptitude install -y xclip"
+
+# meld
+print_checking_install  "meld" "which meld" "sudo apt-get install meld"
+
+print_checking_install  "meld" "which meld" "sudo apt-get install intltool itstool gir1.2-gtksource-3.0 libxml2-utils"
 
 print_finish "Finished: Linux"

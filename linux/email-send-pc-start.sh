b68667a015c3ad9ac0b009352b5ed215a8a7aa20
Eduardo Malherbi <emalherbi@gmail.com>
2017-11-21 17:35:31 -0200

    update email-send.

--- linux/email-send-pc-start.sh
+++ linux/email-send-pc-start.sh
@@ -2,6 +2,6 @@
 
 OS=$(uname -s)
 PC=$(id -un)
-DT=$(date '+%A %W %Y %X')
+DT=$(date '+%A - %x %X')
 
 echo "Hello. Your computer $PC - $OS was connected at $DT." | mail -s "[$PC - $OS] Access $DT" emalherbi@gmail.com

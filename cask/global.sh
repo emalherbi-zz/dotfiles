9fc606277ea370d126bafb7f38583ba5506515db
Eduardo Malherbi <emalherbi@gmail.com>
2018-02-13 23:43:56 -0200

    fix #87

--- cask/global.sh
+++ cask/global.sh
@@ -2,33 +2,15 @@
 
 source common/functions.sh
 
-if which atom &> /dev/null; then
-    print_checked "atom"
-else
-    brew cask install atom
-fi
-
 install  "dbeaver" "brew cask install dbeaver-communit"
 
 install  "google-chrome" "brew cask install google-chrome"
 
-install  "google-chrome-canary" "brew cask install google-chrome-canary"
-
-install  "torbrowser" "brew cask install torbrowser"
-
-install  "imagealpha" "brew cask install imagealpha"
-
-install  "imageoptim" "brew cask install imageoptim"
-
 install  "vlc" "brew cask install vlc"
-
-install  "miro-video-converter" "brew cask install miro-video-converter"
 
 install  "the-unarchiver" "brew cask install the-unarchiver"
 
 install  "transmission" "brew cask install transmission"
-
-install  "ukelele" "brew cask install ukelele"
 
 install  "virtualbox" "brew cask install virtualbox"
 
